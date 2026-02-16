7e2f1ebe66b7c62e97088df20804fa0e33e735fd
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-02-16 15:01:50 +0100

    feat(exercices/easy): add SecondeDansUneAnnee solution

---
+++ Exercices/Easy/SecondesDansUneAnnee.py
@@ -0,0 +1,13 @@
+# Écris un programme Python affichant le nombre de secondes dans 365 jours.
+
+# Tu auras besoin de la fonction print pour afficher le résultat.
+
+# Tu auras aussi besoin de l'opérateur * pour effectuer des multiplications.
+
+# Par exemple, ce code affiche le nombre de secondes dans une journée :
+
+# print(60 * 60 * 24)
+
+print(60 * 60 * 24 * 365)
+
+# Looks good to me! 31536000 seconds per year, that's a lot!!
