13c38001214cef6504eb8b12bf2ea78bbc429a3e
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-02-16 14:56:04 +0100

    feat(exercices/easy): add Bonjour,monde solution

---
+++ Exercices/Easy/Bonjour,monde.py
@@ -0,0 +1,25 @@
+# Bienvenu sur Genepy !
+# Ceci est un exercice d'exemple pour te familiariser avec le système.
+
+# À droite, ici, se trouve habituellement l'énoncé de l'exercice, par exemple : « Écris un programme qui affiche "Hello world!" suivi d'un retour à la ligne ».
+
+# Ton rendu est à rédiger dans l'éditeur de code (noir) à gauche.
+
+# Mais c'est un exercice d'exemple, alors je l'ai déjà rédigé pour toi ☺
+
+# En jaune, en bas à droite de l’éditeur, le bouton « Envoyer » sert à proposer ton code au robot de correction qui le relira, le testera, et t’indiquera si tu as réussi, ou ce que tu dois améliorer.
+
+# Lorsque ton exercice est validé par le robot de correction, tu peux lire les solutions partagées (le bouton du milieu), ou utiliser le bouton Suivant → pour passer à l'exercice suivant.
+
+# Quoi faire ?
+# Modifie le code proposé (change le texte, clique sur le bouton Envoyer, enlève une parenthèse, clique sur Envoyer, remets la parenthèse, clique sur Envoyer, …), pour te familiariser avec la syntaxe du langage et le robot de correction.
+
+# Mais attention, pour valider cet exercice, le robot de correction attend vraiment que ton programme affiche Hello world! !
+
+print("Hello world!")
+
+# Exact ! Votre code affiche :
+
+# Hello world!
+
+# Tu peux essayer l’exercice suivant en cliquant sur le bouton Suivant →, en bas
