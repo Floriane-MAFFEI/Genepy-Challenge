cb75cb15183dbef1a44075ecef110e39006b9c93
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-02-16 14:58:47 +0100

    feat(exercices/easy): add Afficher42 solution

---
+++ Exercices/Easy/Afficher42.py
@@ -0,0 +1,25 @@
+# Écrivez une seule ligne de code affichant le nombre 42.
+
+# Ce qu'on va apprendre
+# Dans cet exercice, on apprend à utiliser une fonction, en essayant la fonction native print.
+
+# Une fonction c'est du code à qui on a donné un nom :
+
+# la fonction print affiche ce qu'on lui donne,
+# la fonction len mesure ce qu'on lui donne,
+# …
+# La syntaxe pour faire fonctionner une fonction s'appelle « un appel de fonction », par exemple pour demander à la fonction print d'afficher la chaîne de caractères "Hello world", la syntaxe est :
+
+# print("Hello world")
+
+# Conseils
+# Tu auras besoin d'un nombre et de la fonction native print().
+
+# Attention, je ne te demande pas d'afficher la chaîne de caractères "42", composée de deux caractères, mais bien le nombre 42, tu n'as donc pas besoin de guillemets dans cet exercice.
+
+# N'hésite pas à cliquer sur le bouton "Envoyer" autant de fois que nécessaire.
+
+print(42)
+
+# 42 is the answer. Well done!
+
