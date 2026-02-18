a3529b72d82593f56a8666117c53c6f011c6befa
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-02-18 14:34:51 +0100

    feat(exercices/easy): add Import solution

---
+++ Exercices/Easy/Import.py
@@ -0,0 +1,17 @@
+# Rédige un script qui affiche la factorielle de 27.
+
+# Utilise la fonction factorial (https://docs.python.org/fr/3/library/math.html#math.factorial) du module math (https://docs.python.org/fr/3/library/math.html).
+
+# Conseil
+# Tu auras besoin d'importer le module math en utilisant le mot clé import :
+
+# import math
+# Le mot-clé import charge un module dans ton script, import math charge donc le module math dans ton script.
+
+# Tu n'as rien besoin d'installer en plus de Python pour tester ça sur ton ordinateur : comme beaucoup de modules math est fourni par Python. (https://docs.python.org/fr/3/tutorial/modules.html#standard-modules)
+
+import math
+
+print(math.factorial(27))
+
+# Wow 27! is 10888869450418352160768000000, that's... bigger than I expected, but you're right!
