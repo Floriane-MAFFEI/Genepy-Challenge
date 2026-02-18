ec2159098296274dc63b37c059012bdcec3a3d78
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-02-18 12:35:34 +0100

    feat(exercices/easy): add operateurs solution

---
+++ Exercices/Easy/Operateurs.py
@@ -0,0 +1,14 @@
+# Rédige un programme Python affichant le résultat de 8958937768937 divisé par 2851718461558.
+
+# Documentation
+# Les types numériques (https://docs.python.org/fr/3/library/stdtypes.html)
+
+print(8958937768937/2851718461558)
+
+# ou
+
+x=8958937768937
+y=2851718461558
+print(x/y)
+
+# Looks good to me! (This is a good pi approximation!)
