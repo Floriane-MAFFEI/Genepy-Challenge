404576fef5c6ea7633d35ba6fd72791a11bff931
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-02-25 12:17:55 +0100

    feat(exercices/easy): add SommeNombrePair solution

---
+++ Exercices/Easy/SommeNombrePairs.py
@@ -0,0 +1,10 @@
+# Affiche la somme de tous les nombres pairs dans l'intervalle [0; 100].
+
+def sum_even_numbers():
+    total = sum(range(0, 101, 2))
+    print(total)
+
+sum_even_numbers()
+
+# Bien !! Bonne réponse ! 🚀
+
