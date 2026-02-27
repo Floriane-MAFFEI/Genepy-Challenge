6281daaa9d7e40094acb9309e4ca507e4310c257
Floriane-MAFFEI <shadowdevzone@gmail.com>
2026-02-27 08:00:10 +0100

    feat(exercices/easy): add Temperatures solution

---
+++ Exercices/Easy/Temperatures.py
@@ -0,0 +1,26 @@
+# Écris deux fonctions pour convertir de Fahrenheit à Celsius et vice-versa.
+
+# Tes fonctions doivent ressembler à :
+
+# def fahrenheit_to_celsius(temp):
+#     ...
+
+# def celsius_to_fahrenheit(temp):
+#     ...
+# Tu trouveras les conversions dans l'article Wikipédia Degré Fahrenheit. (https://fr.wikipedia.org/wiki/Degr%C3%A9_Fahrenheit)
+
+# Exemple
+# Voici un exemple d'utilisation d'une de tes fonctions dans un interpréteur interactif :
+
+# >>> celsius_to_fahrenheit(100)
+# 212.0
+
+def fahrenheit_to_celsius(temp):
+    return (temp - 32) * 5/9
+
+
+def celsius_to_fahrenheit(temp):
+    return temp * 9/5 + 32
+
+# Your temperature conversions looks good to me!
+
